0347e674bb1b70416f7250fd5cef146663bb7ff0
jeremiah <me@jeremiahrogers.com>
2025-03-05 13:35:43 -0500

    remove unused imports

--- src/vendor/mod.rs
+++ src/vendor/mod.rs
@@ -1,6 +1,4 @@
 // all dependencies and hacks from rust-lightning go here
 
-use bitcoin::secp256k1;
-use bitcoin::secp256k1::{PublicKey as BitcoinPublicKey, Secp256k1, SecretKey as BitcoinSecretKey};
 pub use lightning::ln::peer_channel_encryptor::PeerChannelEncryptor;
 pub use lightning::sign::KeysManager;
